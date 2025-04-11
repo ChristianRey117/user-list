a8857bde4d68b207343c5651222f1dfb5a417913
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2025-04-10 21:32:32 -0400

    feat(user-list): get data from api and pass to user information

--- app/components/user-list/user-list.tsx
+++ app/components/user-list/user-list.tsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
 import User from "../user/user";
+import type { IUser } from "../user/IUser";
+import { Grid } from "@mui/material";
 
 export default function UserList() {
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    fetch("http://localhost:3000/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  };
   return (
     <>
-      <p>User list</p>
-
-      <User></User>
+      {users.map((user) => {
+        return (
+          <Grid key={user.id} size={4} spacing={2}>
+            <User email={user.email} id={user.id} name={user.name}></User>
+          </Grid>
+        );
+      })}
     </>
   );
 }
