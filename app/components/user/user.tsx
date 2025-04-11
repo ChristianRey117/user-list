b2f536da43446f15b6dd586d256127f0838b4925
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2025-04-10 21:31:55 -0400

    feat(user): show user information with card

--- app/components/user/user.tsx
+++ app/components/user/user.tsx
@@ -1,7 +1,33 @@
-export default function User() {
+import { CardHeader, Grid } from "@mui/material";
+import type { IUser } from "./IUser";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+
+export default function User(user: IUser) {
   return (
     <>
-      <p>User</p>
+      <Card>
+        <CardHeader title="User information"></CardHeader>
+        <CardContent>
+          <Grid container rowSpacing={2}>
+            <Grid size={3}>
+              <span>Name</span>
+            </Grid>
+
+            <Grid size={9}>
+              <p>{user.name}</p>
+            </Grid>
+
+            <Grid size={3}>
+              <span>Email</span>
+            </Grid>
+
+            <Grid size={9}>
+              <p>{user.email}</p>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
     </>
   );
 }
