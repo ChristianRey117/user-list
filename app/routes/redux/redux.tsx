7f25fd8e159adf48e9f84d69e6da59352b607193
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2025-04-11 02:10:40 -0400

    fix(redux): add login and auth-form and show it if is authenticated

--- app/routes/redux/redux.tsx
+++ app/routes/redux/redux.tsx
@@ -1,17 +1,29 @@
 import { Grid } from "@mui/material";
 import AuthForm from "~/components/auth-form/auth-form";
+import { selectIsAuthenticated } from "~/redux/auth-reducer/auth-reducer";
+import type { RootState } from "~/redux/auth-reducer/store";
+import { useSelector } from "react-redux";
+import Login from "~/components/login/login";
 
 export default function Redux() {
+  const isAuthenticated = useSelector((state: RootState) =>
+    selectIsAuthenticated(state)
+  );
+
   return (
     <>
-      <Grid
-        size={12}
-        justifyContent={"center"}
-        display={"flex"}
-        paddingTop={"25%"}
-      >
-        <AuthForm></AuthForm>
-      </Grid>
+      {isAuthenticated ? (
+        <Login></Login>
+      ) : (
+        <Grid
+          size={12}
+          justifyContent={"center"}
+          display={"flex"}
+          paddingTop={"10%"}
+        >
+          <AuthForm></AuthForm>
+        </Grid>
+      )}
     </>
   );
 }
