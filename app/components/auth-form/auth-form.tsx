cfb367fee1e8323d8c8226946d5680ba7a130f13
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2025-04-11 02:09:54 -0400

    feat(auth-form): set user information to login reduce

--- app/components/auth-form/auth-form.tsx
+++ app/components/auth-form/auth-form.tsx
@@ -1,20 +1,42 @@
 import "./auth-form.css";
 import { useState } from "react";
+import { login } from "~/redux/auth-reducer/auth-reducer";
+import { useSelector, useDispatch } from "react-redux";
+import type { AppDispatch } from "~/redux/auth-reducer/store";
 
 export default function AuthForm() {
   const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-  const handleLogin = () => {};
+  const dispatch = useDispatch<AppDispatch>();
+  const handleLogin = () => {
+    const userData = { username };
+    dispatch(login(userData));
+  };
   return (
     <>
-      <form className="form">
+      <form
+        className="form"
+        onSubmit={(event) => {
+          event.preventDefault();
+          handleLogin();
+        }}
+      >
         <p className="form-title">Sign in to your account</p>
         <div className="input-container">
-          <input type="email" placeholder="Enter email" />
+          <input
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => setUsername(e.target.value)}
+          />
           <span></span>
         </div>
         <div className="input-container">
-          <input type="password" placeholder="Enter password" />
+          <input
+            type="password"
+            placeholder="Enter password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <button type="submit" className="submit">
           Sign in
